678c159cdc08acf05002b87fcd0e2b2db8eee71a
Neeraj Pandey <61829291+neeraj06-09@users.noreply.github.com>
2022-01-24 15:31:30 +0530

    Update Menu.js

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -12,7 +12,7 @@ const Menu = () => {
             </div>
           </div>
           <div className="col-6 p-25">
-            <h1>Welcome To Us Pizza</h1>
+            <h1>Welcome To Pizza Master</h1>
             <p>
             Pizza, dish of Italian origin consisting of a flattened disk of bread dough topped with some combination of olive oil, oregano, tomato, olives, mozzarella or other cheese, and many other ingredients, baked quickly—usually, in a commercial setting, using a wood-fired oven heated to a very high temperature—and served hot.
             
